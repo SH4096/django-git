914ff828a68d45340403349f0e6179aae8408bff
shf <shf1138599894@163.com>
2023-07-24 09:23:35 -0600

    增加查询用户数量视图

---
+++ meiduo/apps/users/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import path
+from apps.users import views
+
+urlpatterns = [
+    path('usernames/<username>/count', views.UsernameCountView.as_view()),
+]
--- meiduo/apps/users/views.py
+++ meiduo/apps/users/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.views import View
+from django.http import JsonResponse
+from apps.users import models
 
-# Create your views here.
+
+class UsernameCountView(View):
+    def get(self, request, username):
+        count = models.User.objects.filter(username=username).count()
+        return JsonResponse({'code': 0, 'count': count, 'errors': 'ok'})
